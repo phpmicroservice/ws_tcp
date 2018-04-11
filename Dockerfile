1980abcf75e8208963b3c6f07bb3841abb075f72
Dongasai <1514582970@qq.com>
2018-04-11 18:01:42 +0800

    2018年4月11日17:51:48

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM daocloud.io/1514582970/pms_docker_php:cli71_swoole_phalcon
+
+MAINTAINER      Dongasai "1514582970@qq.com"
+
+RUN apt update;apt install -y vim
+COPY . /var/www/html/
+ENV TCP_SERVER_HOST="pms_proxy"
+ENV TCP_SERVER_PORT="9502"
+EXPOSE 9502
+WORKDIR /var/www/html/
+CMD php start.php
+
--- start.php
+++ start.php
@@ -1,6 +1,16 @@
 <?php
-define('PACKAGE_EOF', '_pms_');
+/**
+ * 获取环境变量的方法
+ * @param $name
+ * @param string $default
+ * @return array|false|string
+ */
+function get_env($name, $default = '')
+{
+    return getenv(strtoupper($name)) === false ? $default : getenv(strtoupper($name));
+}
 
-define('TCP_SERVER_HOST', 'pms_proxy');
-define('TCP_SERVER_PORT', 9502);
+define('PACKAGE_EOF',get_env('PACKAGE_EOF', '_pms_'));
+define('TCP_SERVER_HOST', get_env('TCP_SERVER_HOST','pms_proxy'));
+define('TCP_SERVER_PORT', get_env('TCP_SERVER_PORT',9502));
 include 'client.php';
