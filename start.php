d87705169464b1ced3eba49c9ca2895cc50ec554
dongasai <1514582970@qq.com>
2019-11-13 08:55:54 +0800

    修正

--- start.php
+++ start.php
@@ -5,6 +5,6 @@ define('ROOT_DIR', __DIR__);
 require ROOT_DIR . '/vendor/autoload.php';
 
 
-define('TCP_SERVER_HOST', \pms\get_env('TCP_SERVER_HOST','pms_proxy'));
+define('TCP_SERVER_HOST', \pms\get_env('TCP_SERVER_HOST','pms_proxy_host'));
 define('TCP_SERVER_PORT', \pms\get_env('TCP_SERVER_PORT',9502));
 include 'client.php';
